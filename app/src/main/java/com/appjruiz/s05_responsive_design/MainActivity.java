0e2b03c8c8adec6b7bf5187e43cd29de4ab6602d
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-10-12 02:59:35 -0300

    45. S05-CL05: Bottom Navigation View

--- app/src/main/java/com/appjruiz/s05_responsive_design/MainActivity.java
+++ app/src/main/java/com/appjruiz/s05_responsive_design/MainActivity.java
@@ -1,14 +1,28 @@
 package com.appjruiz.s05_responsive_design;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        // definir el evento click sobre el boton de Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotesActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
